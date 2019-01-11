6d2cea6e47eed8b008d256c2e3a784293c64028d
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-11 18:38:32 +0400

    Fix pep8 notices

--- bars.py
+++ bars.py
@@ -2,20 +2,25 @@ import requests
 
 default_bars_url = 'https://devman.org/fshare/1503831681/4/'
 
+
 def get_bar_name(bar):
     return bar['properties']['Attributes']['Name']
 
+
 def get_bar_seatscount(bar):
-    return  bar['properties']['Attributes']['SeatsCount']
+    return bar['properties']['Attributes']['SeatsCount']
+
 
 def get_bar_coordinates(bar):
-    return  bar['geometry']['coordinates']
+    return bar['geometry']['coordinates']
+
 
 def get_default_latlng():
     r = requests.get('https://api.userinfo.io/userinfos')
     user_data = r.json()
     position = user_data['position']
     return [position['latitude'], position['longitude']]
+
 
 def get_distance(coordinates1, coordinates2):
     [lat1, lng1] = coordinates1
@@ -24,9 +29,11 @@ def get_distance(coordinates1, coordinates2):
     lng_distance = abs(lng1 - lng2)
     return (lat_distance**2 + lng_distance**2)**0.5
 
+
 def load_data(filepath):
     r = requests.get(filepath)
-    return r.json()['features'];
+    return r.json()['features']
+
 
 def coordinates_to_str(coordinates):
     return ','.join(map(str, coordinates))
@@ -43,14 +50,20 @@ def get_smallest_bar(data):
 
 
 def get_closest_bar(data, longitude, latitude):
-    data.sort(key=lambda x: get_distance(get_bar_coordinates(x), [latitude, longitude]))
+    data.sort(key=lambda x: get_distance(
+        get_bar_coordinates(x), [latitude, longitude]))
     return data[0]
 
-def main():
-    file_url = input('Which url should we use to load data? (or just enter to use {}) - '.format(default_bars_url)) or  default_bars_url
 
-    default_latlng_str =coordinates_to_str(get_default_latlng())
-    latlng_str = input('Please enter your coordinates in format `lat, lng` (like `{}`) or just press enter - '.format(default_latlng_str)) or default_latlng_str
+def main():
+    file_url = input('Url to load data? (or just enter to use {}) - '.format(
+        default_bars_url)) or default_bars_url
+
+    default_latlng_str = coordinates_to_str(get_default_latlng())
+    latlng_str = input('''
+Your coordinates in format `lat, lng` or just press enter
+We detected your  coordinates as `{}` - '''.strip().format(
+        default_latlng_str)) or default_latlng_str
 
     [s_lat, s_lng] = latlng_str.split(',')
     lat = float(s_lat.strip())
@@ -62,12 +75,15 @@ def main():
     smallest_bar = get_smallest_bar(bars_data)
     closest_bar = get_closest_bar(bars_data, lat, lng)
 
-    print('Biggest bar is `{}` with {} seats'.format(get_bar_name(biggest_bar), get_bar_seatscount(biggest_bar)))
-    print('Smallest bar is `{}` with {} seats'.format(get_bar_name(smallest_bar), get_bar_seatscount(smallest_bar)))
+    print('Biggest bar is `{}` with {} seats'.format(
+        get_bar_name(biggest_bar), get_bar_seatscount(biggest_bar)))
+    print('Smallest bar is `{}` with {} seats'.format(
+        get_bar_name(smallest_bar), get_bar_seatscount(smallest_bar)))
 
-    closest_bar_coordinates = coordinates_to_str(get_bar_coordinates(closest_bar))
-    print('Closest bar is `{}` with  coordinates {}'.format(get_bar_name(closest_bar), closest_bar_coordinates))
-
+    closest_bar_coordinates = coordinates_to_str(
+        get_bar_coordinates(closest_bar))
+    print('Closest bar is `{}` with  coordinates {}'.format(
+        get_bar_name(closest_bar), closest_bar_coordinates))
 
 
 if __name__ == '__main__':
